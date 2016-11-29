24ee132158f3d669d2916aef02e349084a28395d
DenInBlackOHS <DenInBlackOHS@gmail.com>
2016-11-28 20:15:17 -0500

    New Version

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sound_Test.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Ethan on 10/3/2016.
+ */
+
+//@Autonomous(name="Omni: Sound Test", group ="Auto")
+
+public class Sound_Test extends OmniAutoClass {
+
+    //Context context;
+
+    //MediaPlayer mp = MediaPlayer.create(context, R.raw.worldToChange);
+
+    public void runOpMode() throws InterruptedException {
+
+        //waitForStart();
+
+        //mp.setVolume(1, 1);
+
+        //mp.start();
+    }
+
+}
