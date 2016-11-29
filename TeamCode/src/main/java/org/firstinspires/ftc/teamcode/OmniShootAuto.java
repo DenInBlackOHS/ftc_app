c20c9ad4d0962c19960a74457f9dc22f98b5314c
DenInBlackOHS <DenInBlackOHS@gmail.com>
2016-11-28 20:15:17 -0500

    New Version

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniShootAuto.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by Ethan on 11/19/2016.
+ */
+
+@Autonomous(name="Omni: ShootOnly", group ="Auto")
+
+public class OmniShootAuto extends OmniAutoClass {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        setupRobotParameters(4, 20);
+
+        waitForStart();
+
+        shoot(.35, 4000);
+    }
+}
